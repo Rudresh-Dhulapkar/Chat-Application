9c4e1582c9430ebc95d4652dc50be669a125ccfd
Rudresh <83804183+Rudresh-Dhulapkar@users.noreply.github.com>
2024-04-21 10:34:31 +0530

    Javascript Done

--- javaScript/script.js
+++
@@ -1,7 +0,0 @@
-// $(document).ready(function() {
-//     $('.content').hover(function() {
-//       $(this).css('background-color', 'grey');
-//     }, function() {
-//       $(this).css('background-color', 'white'); 
-//     });
-//   });
